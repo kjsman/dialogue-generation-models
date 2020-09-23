faa4f1a230229330fe47b180bb7d508dca7f59ab
noowad93 <da0968@naver.com>
2020-09-23 19:39:10 +0900

    chore: 레포 초기 세팅 추가

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dialogue-generation-models",
+    version="0.0.1",
+    description="Pingpong Dialogue Generation Models",
+    install_requires=[
+        "torch==1.6.0",
+        "sentencepiece==0.1.91",
+        "tensorflow==2.1.0",
+        "transformers==3.1.0",
+    ],
+    url="https://github.com/pingpong-ai/dialogue-generation-models.git",
+    author="ScatterLab",
+    author_email="developers@scatterlab.co.kr",
+    packages=find_packages(exclude=["tests"]),
+    license="MIT License",
+)
